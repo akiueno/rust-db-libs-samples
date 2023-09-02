21916ea8fefeac3768ae5bc2a93a3acc6770fcb3
AkihiroUeno <akihiro.ueno.2@gmail.com>
2023-09-02 19:18:33 +0900

    migrationファイルをrevertできるように修正した

---
+++ sqlx/migrations/20230902083226_product.down.sql
@@ -0,0 +1,4 @@
+-- Add down migration script here
+DROP TABLE sqlx.product;
+DROP TABLE sqlx.product_category;
+DROP SCHEMA sqlx;
--- sqlx/migrations/20230902083226_product.up.sql
+++ sqlx/migrations/20230902083226_product.up.sql
@@ -1,16 +1,21 @@
+-- Add up migration script here
 CREATE SCHEMA sqlx;
 
 CREATE TABLE sqlx.product_category (
   id BIGSERIAL NOT NULL,
-  name varchar(20) not null default '',
+  name varchar(20) NOT NULL DEFAULT '',
+  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   CONSTRAINT product_category_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE sqlx.product (
   id BIGSERIAL NOT NULL,
-  name varchar(20) not  null default '',
-  price integer not null default 0,
+  name varchar(20) NOT NULL DEFAULT '',
+  price integer NOT NULL DEFAULT 0,
   category_id BIGINT NOT NULL,
+  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   CONSTRAINT product_pk PRIMARY KEY (id),
   CONSTRAINT product_category_fk FOREIGN KEY(category_id)
     REFERENCES sqlx.product_category(id)
